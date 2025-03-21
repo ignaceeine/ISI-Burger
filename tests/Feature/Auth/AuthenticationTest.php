cc3052483439afe7d70f1c5ecc580ea9c2fa66b1
ignaceeine <ignaceeine@gmail.com>
2025-03-21 08:08:45 +0000

    +++++++++

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -39,7 +39,14 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => 'Test User3',
+            'telephone' => '9123456789',
+            'email' => 'test3@example.com',
+            'adresse' => 'Test Adresse',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
 
         $this->post('/login', [
             'email' => $user->email,
@@ -51,7 +58,14 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_logout(): void
     {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => 'Test User4',
+            'telephone' => '8123456789',
+            'email' => 'test4@example.com',
+            'adresse' => 'Test Adresse',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
 
         $response = $this->actingAs($user)->post('/logout');
 
