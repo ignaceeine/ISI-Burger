d92ed5c6d3745634a3ff23819227b929f5c32690
ignaceeine <ignaceeine@gmail.com>
2025-03-21 14:50:25 +0000

    ++++++++

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Ignace DIATTA">
+    <meta name="author" content="Ignace E. DIATTA">
     <meta name="description" content="ISI-BURGER - Fast Food Restaurant">
     <!--favicon-->
     <link rel="icon" href="{{ URL::asset('img/logo/ISI-burger-sf.png') }}" type="image/png">
